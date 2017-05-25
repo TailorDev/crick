de34583aa2261672362d81918f75bcf317b03620
William Durand <will+git@drnd.me>
2017-05-25 16:31:12 +0200

    [web] talk to the API

--- web/src/App.js
+++ web/src/App.js
@@ -1,18 +1,49 @@
+/* @flow */
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props: Object) {
+    super(props);
+
+    this.state = {
+      fullname: '',
+    };
+  }
+
+  state: {
+    fullname: string,
+  };
+
+  componentDidMount() {
+    fetch('/users/me')
+      .then(response => {
+        return response.json()
+      })
+      .then(json => {
+        this.setState({
+          fullname: json.fullname,
+        });
+      });
+  }
+
   render() {
+    const hello = this.state.fullname === '' ? '' : (
+      <p>
+        Hello, <strong>{this.state.fullname}</strong>!
+      </p>
+    );
+
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="App-intro">
+          {hello}
+        </div>
       </div>
     );
   }
