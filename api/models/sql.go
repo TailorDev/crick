0bcb56cef8524e829d5689a683a6cc5b323f8d7b
William Durand <will+git@drnd.me>
2017-05-31 21:48:58 +0200

    [api] test QueryBuilder (and fix it)

--- api/models/sql.go
+++ api/models/sql.go
@@ -5,90 +5,112 @@ import (
 	"strings"
 )
 
+// QueryBuilder represents a SQL query that is built by calling functions to
+// it.
 type QueryBuilder struct {
-	Select []string
-	From   []string
-	Where  []string
-	Join   []string
-	Offset int
-	Limit  int
-	Values []interface{}
+	selects []string
+	froms   []string
+	wheres  []string
+	joins   []string
+	offset  int
+	limit   int
+	values  []interface{}
 }
 
+// Returns a new QueryBuilder.
 func NewQueryBuilder() QueryBuilder {
 	return QueryBuilder{
-		Select: []string{},
-		From:   []string{},
-		Where:  []string{},
-		Join:   []string{},
-		Values: []interface{}{},
-		Limit:  0,
-		Offset: 0,
+		selects: []string{},
+		froms:   []string{},
+		wheres:  []string{},
+		joins:   []string{},
+		values:  []interface{}{},
+		limit:   0,
+		offset:  0,
 	}
 }
 
-func (q *QueryBuilder) AddSelect(clause string) {
-	q.Select = append(q.Select, clause)
+// AddSelect adds a SELECT clause to the query.
+func (q *QueryBuilder) AddSelect(clause string) *QueryBuilder {
+	q.selects = append(q.selects, clause)
+	return q
 }
 
-func (q *QueryBuilder) AddFrom(clause string) {
-	q.From = append(q.From, clause)
+// AddFrom adds a FROM clause to the query.
+func (q *QueryBuilder) AddFrom(clause string) *QueryBuilder {
+	q.froms = append(q.froms, clause)
+	return q
 }
 
-func (q *QueryBuilder) AddWhere(clause string, val interface{}) {
-	q.Where = append(q.Where, clause)
-	q.Values = append(q.Values, val)
+// AddWhere adds a WHERE clause to the query.
+func (q *QueryBuilder) AddWhere(clause string, val interface{}) *QueryBuilder {
+	q.wheres = append(q.wheres, clause)
+	q.values = append(q.values, val)
+	return q
 }
 
-func (q *QueryBuilder) AddJoin(clause string) {
-	q.Join = append(q.Join, clause)
+// AddJoin adds a JOIN clause to the query.
+func (q *QueryBuilder) AddJoin(clause string) *QueryBuilder {
+	q.joins = append(q.joins, clause)
+	return q
 }
 
-func (q *QueryBuilder) Paginate(page, limit int) {
-	q.Offset = (page - 1) * limit
-	q.Limit = limit
+// Paginate adds limit and offset to the query.
+func (q *QueryBuilder) Paginate(page, limit int) *QueryBuilder {
+	q.offset = (page - 1) * limit
+	q.limit = limit
+	return q
 }
 
+// Values returns the parameter values of the query.
+func (q *QueryBuilder) Values() []interface{} {
+	return q.values
+}
+
+// ToSQL returns the resulting SQL query as a string.
 func (q *QueryBuilder) ToSQL() string {
 	sql := fmt.Sprintf(
 		"SELECT %s FROM %s",
-		strings.Join(q.Select, ", "),
-		strings.Join(q.From, ", "),
+		strings.Join(q.selects, ", "),
+		strings.Join(q.froms, ", "),
 	)
 
-	if len(q.Join) > 0 {
-		sql = fmt.Sprintf("%s %s", sql, strings.Join(q.Join, " "))
+	if len(q.joins) > 0 {
+		sql = fmt.Sprintf("%s %s", sql, strings.Join(q.joins, " "))
 	}
 
-	if len(q.Where) > 0 {
+	if len(q.wheres) > 0 {
 		where := []string{}
-		for k, clause := range q.Where {
+		for k, clause := range q.wheres {
 			where = append(where, strings.Replace(clause, "?", fmt.Sprintf("$%d", (k+1)), 1))
 		}
 
 		sql = fmt.Sprintf("%s WHERE %s", sql, strings.Join(where, " AND "))
 	}
 
-	if q.Limit > 0 && q.Offset > 0 {
-		sql = fmt.Sprintf("%s LIMIT %d OFFSET %d", q.Limit, q.Offset)
+	if q.limit > 0 && q.offset >= 0 {
+		sql = fmt.Sprintf("%s LIMIT %d OFFSET %d", sql, q.limit, q.offset)
 	}
 
 	return sql
 }
 
+// ToCountSQL returns the COUNT SQL query as a string, which does not contain
+// any LIMIT/OFFSET, and the SELECT clause is `COUNT(*)`. This is useful to
+// retrieve the number of results of a given query.
 func (q *QueryBuilder) ToCountSQL() string {
 	sql := fmt.Sprintf(
-		"SELECT COUNT(frames.*) FROM %s",
-		strings.Join(q.From, ", "),
+		"SELECT COUNT(*) FROM %s",
+		strings.Join(q.froms, ", "),
 	)
 
-	if len(q.Join) > 0 {
-		sql = fmt.Sprintf("%s %s", sql, strings.Join(q.Join, " "))
+	if len(q.joins) > 0 {
+		sql = fmt.Sprintf("%s %s", sql, strings.Join(q.joins, " "))
 	}
 
-	if len(q.Where) > 0 {
+	if len(q.wheres) > 0 {
 		where := []string{}
-		for k, clause := range q.Where {
+		for k, clause := range q.wheres {
 			where = append(where, strings.Replace(clause, "?", fmt.Sprintf("$%d", (k+1)), 1))
 		}
 
