2a54a43bb39c3e9a2d4530b76b7f8de2c1ba858b
Julien Maupetit <julien@tailordev.fr>
2017-08-01 14:26:03 +0200

    Add draft content for unlogged users home

--- web/src/Home/NotConnected.js
+++ web/src/Home/NotConnected.js
@@ -1,9 +1,38 @@
 /* @flow */
 import React from 'react';
+import { Link } from 'react-router-dom';
+import CrickLogoImg from './img/logo-crick.png';
 
 const NotConnected = () =>
-  <div>
-    Login to continue.
+  <div className="wrapper">
+    <header className="brand">
+      <div className="logo">
+        <img src={CrickLogoImg} alt="Crick Logo" />
+      </div>
+      <div className="title">
+        <h1>
+          <Link to="/">
+            <span>app.</span>
+            crick
+            <span>.io</span>
+          </Link>
+        </h1>
+        <div className="baseline">
+          An <a href="https://github.com/TailorDev/crick">open-source</a> backend
+          for <a href="https://github.com/TailorDev/Watson">Watson</a>, the CLI
+          to track your time.
+        </div>
+      </div>
+    </header>
+
+    <div className="content">
+      Login now to start using Crick!
+    </div>
+
+    <footer className="copyright">
+      Handcrafted by the good people
+      at <a href="https://tailordev.fr">TailorDev</a>.
+    </footer>
   </div>;
 
 export default NotConnected;
